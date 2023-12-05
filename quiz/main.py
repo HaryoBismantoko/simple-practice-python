c9c96a41ceddf7acc6406e4504a91e0486b8cdc9
Haryo Bismantoko <92304963+HaryoBismantoko@users.noreply.github.com>
2023-12-05 13:57:06 +0700

    Update main.py

--- quiz/main.py
+++ quiz/main.py
@@ -1,20 +1,13 @@
-from data import question_data
-score = 0
-for quesAsk in question_data:
-    for key, value in quesAsk.items():
-        if key == 'text':
-            print(value)
-        elif key == 'answer':
-            answer = value
-            inpQuest = input("true / false : ")
-            if inpQuest.lower() == answer.lower():
-                print("Answer is right")
-                score +=1
-                print(f"Your score is {score}")
-            elif inpQuest.lower() != answer.lower():
-                print("Answer is wrong")
+from logic import allLogic
+from data import questionData
 
-print(f"\n \033[1mYour final score is {score}\033[0m")
+def mainlogic():
+    logic = allLogic()
+    logic.set_sub_quest(questionData)
+    logic.process_sub_quest()
+
+mainlogic()
+
 
 
 
